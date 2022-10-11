4ef1e8eef19350597201ffc970983070adb9da9d
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-10-11 18:43:29 -0300

    requisito 3 realizado

--- src/loginRouter.js
+++ src/loginRouter.js
@@ -3,16 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const geradorToken = require('./geradorToken.js');
-const { validator } = require('./validator');
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
   const token = geradorToken();
-   const { email, password } = req.body;
-  const validateEmail = Object.values(validator(email)).length;
-  const validatePassword = Object.values(validator(password)).length;
-  if (validateEmail === 0 || validatePassword === 0) {
-    res.status(400).json({ message: '"Email" e "Senha" são campos obrigatórios' });
-  }
   res.status(200).json({ token });
 });
 
