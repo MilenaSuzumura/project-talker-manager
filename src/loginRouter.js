c18079e9a3a9af90a4f9a9e02e6a26744cd202b2
MilenaSuzumura <milena.suzumura11@gmail.com>
2022-10-11 19:56:54 -0300

    fiz o requisito 4(FINALMENTE!!)

--- src/loginRouter.js
+++ src/loginRouter.js
@@ -5,17 +5,8 @@ const router = express.Router();
 const geradorToken = require('./geradorToken.js');
 const { validaEmail, validaSenha } = require('./validator');
 
-router.post('/', async (req, res) => {
+router.post('/', validaEmail, validaSenha, async (_req, res) => {
   const token = geradorToken();
-  const { email, password } = req.body;
-  const emailValida = validaEmail(email);
-  const senhaValida = validaSenha(password);
-  if (emailValida) {
-    res.status(400).json(emailValida.message);
-  }
-  if (senhaValida) {
-    res.status(400).json(senhaValida.message);
-  }
   res.status(200).json({ token });
 });
 
--- src/validator.js
+++ src/validator.js
@@ -1,19 +1,23 @@
-const validaSenha = (senha) => {
-  if (senha.length === 0) {
-    return { message: 'O campo "password" é obrigatório' };
+const validaSenha = (req, res, next) => {
+    const { password } = req.body;
+  if (password === undefined) {
+    res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }  
+  if (password.length < 6) {
+    res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
-  if (senha > 6) {
-    return { message: 'O "password" deve ter pelo menos 6 caracteres' };
-  }
+  next();
 };
 
-const validaEmail = (email) => {
-  if (email.length === 0) {
-    return { message: 'O campo "email" é obrigatório' };
+const validaEmail = (req, res, next) => {
+  const { email } = req.body;
+  if (email === undefined) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
-  if (email.includes('@') && email.endsWith('.com')) {
-    return { message: 'O "email" deve ter o formato "email@email.com"' };
+  if (!email.includes('@') || !email.endsWith('.com')) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
+  next();
 };
 
 module.exports = { validaSenha, validaEmail };
